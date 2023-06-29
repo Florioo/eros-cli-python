ef2f7990c45fbe0bee2500266bf934c02b682b63
Floris Vernieuwe <floris@vernieuwe.eu>
2023-06-29 15:09:27 +0200

    Linked the requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements("requirements.txt")
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='eros-view',
     version='0.1.0',
     py_modules=['main'],
-    install_requires=[
-        'Click',
-    ],
+    install_requires=reqs,
     entry_points={
         'console_scripts': [
             'eros-view = main:eros_log',
