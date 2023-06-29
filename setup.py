54e2340c9f33a9cee15accbe7e061b76e9087520
Floris Vernieuwe <floris@vernieuwe.eu>
2023-06-29 15:14:38 +0200

    Fixed issue

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ try: # for pip >= 10
     from pip._internal.req import parse_requirements
 except ImportError: # for pip <= 9.0.3
     from pip.req import parse_requirements
-    
-install_reqs = parse_requirements("requirements.txt")
+
+install_reqs = parse_requirements("requirements.txt", session=False)
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
