66245f53aae65f686ba1e1ed080386d6aded4ade
Floris Vernieuwe <floris@vernieuwe.eu>
2023-07-01 14:15:06 +0200

    moved impors

--- src/eros_cli/transport_selection.py
+++ src/eros_cli/transport_selection.py
@@ -1,16 +1,6 @@
 from eros import Eros, ErosSerial,ErosUDP,ErosTCP,ErosZMQ
 import time
 import click
-from .app.app_log  import app_log
-from .app.app_cli  import app_cli 
-from .app.app_zmq  import app_zmq
-from .app.app_perf import app_perf
-# from .app_zmq import zmq as app_zmq
-# from .app_test import test as app_test
-from .app.app_machine_cli import app_machine_cli
-from .app.app_dump import app_dump
-
-
 
 @click.group(chain=True)
 @click.option('--port', default="auto", help='Port to use for UART communication')
@@ -78,6 +68,13 @@ def attach_app(app):
     tcp.add_command(app)
     udp.add_command(app)
     zmq.add_command(app)
+    
+from .app.app_log  import app_log
+from .app.app_cli  import app_cli 
+from .app.app_zmq  import app_zmq
+from .app.app_perf import app_perf
+from .app.app_machine_cli import app_machine_cli
+from .app.app_dump import app_dump
 
 attach_app(app_log)
 attach_app(app_cli)
